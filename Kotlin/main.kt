dfb5d0de72c70e517148464685134c406e8d1ec9
TikkaToRide <34696597+TikkaToRide@users.noreply.github.com>
2020-08-29 22:35:58 +0930

    Update main.kt

--- Kotlin/main.kt
+++ Kotlin/main.kt
@@ -4,28 +4,30 @@ import kotlin.math.pow
 
 fun main() {
 
-    val principal = 154340
-    val payment = 35316  //this is the repayment amount
-    val numberOfYears = 5  //how many years is the loan for?
-    val repayFreq = 1  //how many payments in the year eg. 1 = yearly, 12 = monthly
+    print("Loan Amount $")
+    val principal = readLine()!!.toDouble()
+    print("Repayment Amount $")
+    val payment = readLine()!!.toDouble()
+    print("How Many Years of Loan: ")
+    val numberOfYears = readLine()!!.toDouble()
+    print("Payment Frequency: ")
+    val repayFreq = readLine()!!.toDouble()
     val numPay = numberOfYears * repayFreq
 
-    val error = 10.00.pow(-5.00)
-    var approx = 0.05/12  //start with a guess that the APR is 5%
+    val error = 10.0.pow(-5.0)
+    var approx = 0.05/12 //let's start with a guess that the APR is 5%
     var prevApprox: Double
 
     fun f(x: Double): Double {
-        val base: Double = 1 + x
-        return principal * x * base.pow(numPay) / (base.pow(numPay) - 1 ) - payment
+        return principal * x * (1 + x).pow(numPay) / ((1 + x).pow(numPay) - 1 ) - payment
     }
 
     fun fPrime(x: Double): Double{
-        val base: Double = 1 + x
-        return principal * (base.pow(numPay) / (-1.00 + base.pow(numPay)) -
-                numPay * x * base.pow((-1.00 + 2.00 * numPay)) /
-                (-1.00 + base.pow(numPay)).pow(2.00) +
-                numPay * x * base.pow((-1.00 + numPay)) /
-                (-1.00 + base.pow(numPay)))
+        return principal * ((1 + x).pow(numPay) / (-1 + (1 + x).pow(numPay)) -
+                numPay * x * (1 + x).pow((-1 + 2 * numPay)) /
+                (-1 + (1 + x).pow(numPay)).pow(2) +
+                numPay * x * (1 + x).pow((-1 + numPay)) /
+                (-1 + (1 + x).pow(numPay)))
     }
 
     var k = 0
@@ -38,8 +40,8 @@ fun main() {
         k += 1
     }
 
-    val interestRate = (approx * repayFreq * 10000)/100  //this way we get APRs like 7.5% or 6.55%
+    val interestRate = (approx * repayFreq * 10000)/100 // this way we get APRs like 7.5% or 6.55%
     println("Interest Rate is ${"%.2f".format(interestRate)}%")
-    print(approx)  //used (approx) for the interest rate for the amortisaiton schedule
+    println(approx) // this figure is what is used for the iterest rate for the amortisation schedue
 
 }
